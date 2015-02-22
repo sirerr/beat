e795d526748a59463d9a9ee8ecf38fef43452564
sirerr <sirerr@gmail.com>
2015-02-22 04:10:57 -0500

    wip, trying to get line working

---
+++ Beat-Unity/Assets/Scripts/Lineaction.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lineaction : MonoBehaviour {
+
+	//raycast variables
+	private bool ownerchosen = false;
+	public int lineowner = 0;
+	public int randomrot =0;
+
+	//testcode
+	public bool firstime = true;
+	//testcode
+	//line variables
+	public GameObject centerarrow;
+	public Material ownermat;
+	public Material takermat;
+
+
+	//centerarrow code
+	//collection of rotations
+	public float[] centerarrowrotation;
+	//opponent speed coroutine wait time
+	public float opspeed =0;
+	//location in rotation array
+	public int chosenrotationcounter =0;
+	//original rotation of arrow
+	private Quaternion originalrotation;
+
+
+
+	// Use this for initialization
+ void Start () {
+ 
+		randomrot = Random.Range(0,centerarrowrotation.Length -1);
+
+ 	}
+	
+ 
+	void Update () {
+
+	}
+
+	public void hitbyray()
+
+	{
+		Debug.Log ("I was hit by a Ray");
+
+		if(!ownerchosen)
+		{		
+		centerarrow = gameObject.transform.FindChild("linearrow").gameObject;
+		originalrotation = centerarrow.transform.rotation;
+		
+		ownerchosen = true;
+		
+	 centerarrow.transform.Rotate(0,centerarrowrotation[randomrot],0);
+ 
+			linebehavoir();
+		}
+
+	}
+
+
+	public void linebehavoir()
+	{
+
+
+		RaycastHit hitout;
+
+		if(Physics.Raycast(centerarrow.transform.position,centerarrow.transform.TransformDirection(Vector3.forward), out hitout))
+		{
+			print ("looking out there");
+
+			if(hitout.transform.GetComponent<Lineaction>().lineowner == 0)
+			{	
+				hitout.transform.renderer.material = hitout.transform.GetComponent<Lineaction>().ownermat;
+				hitout.transform.GetComponent<Lineaction>().lineowner = 2;
+				hitout.transform.SendMessage ("hitbyray");
+			}
+			else
+			{
+				if(firstime){
+				chosenrotationcounter = randomrot +1;
+					firstime = false;
+				}
+
+			}
+		
+			if(chosenrotationcounter> centerarrowrotation.Length -1)
+			{
+				chosenrotationcounter = 0; 
+			}
+		}
+
+		centerarrow.transform.Rotate(0,centerarrowrotation[chosenrotationcounter],0);
+		StartCoroutine(Opponentspeed());
+
+	}
+
+
+	IEnumerator Opponentspeed()
+	{
+		yield return new WaitForSeconds (opspeed);
+ 
+		chosenrotationcounter =  chosenrotationcounter +1;
+		centerarrow.transform.rotation = originalrotation;
+	 
+		linebehavoir();
+
+	}
+
+
+
+}
---
+++ Beat-Unity/Assets/Scripts/Lineactivator.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lineactivator : MonoBehaviour {
+
+ 
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			activator();
+		}
+
+	}
+
+	public void activator()
+
+	{
+		RaycastHit hitout;
+
+		if(Physics.Raycast(transform.position,Vector3.forward, out hitout))
+		{
+
+			if( hitout.transform.gameObject.transform.renderer.material.name == "notchosenmat (Instance)")
+			{
+			
+				hitout.transform.renderer.material = hitout.transform.GetComponent<Lineaction>().ownermat;
+				hitout.transform.GetComponent<Lineaction>().lineowner = 2;
+			}
+
+			hitout.transform.SendMessage ("hitbyray");
+		}
+
+	}
+}
